906f219c06b974a7ebac3d7debb6a23e69bc2b8b
manzorsarahi <manzorsarahi@gmail.com>
2020-07-10 16:06:39 -0400

    Blank Project

---
+++ Source/GMTK2020Editor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class GMTK2020EditorTarget : TargetRules
+{
+	public GMTK2020EditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange( new string[] { "GMTK2020" } );
+	}
+}
